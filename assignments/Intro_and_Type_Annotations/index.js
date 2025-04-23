18a16d47336727a816c58f35e1da9073ea7f34f3
BadrKali <badrkali1997@gmail.com>
2025-04-23 12:56:36 +0100

    assignments 1 - 11

---
+++ assignments/Intro_and_Type_Annotations/index.js
@@ -0,0 +1,18 @@
+// assignments 1 
+// function calculate(numOne: number, numTwo: number): number {
+//     return numOne + numTwo;
+//   }
+//   console.log(calculate(10, 20)); // 30
+//   console.log(calculate("10", "20")); // We Don't Need This To Work
+//   console.log(calculate(+true, +true)); // 2
+// assignments 2
+// function printInfo(valueOne: number | string , valueTwo: number | string): string {
+//     return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+//   }
+//   console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+//   console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+//   console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+// assignments 3
+var arr;
+arr = [1, [true, false], ["ogna", ["ogna", 1]]];
+console.log("arr", arr);
